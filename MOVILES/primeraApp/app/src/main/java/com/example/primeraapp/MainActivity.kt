8dcf8f51f3bc23f5a5dbe6d48e76284d5eee0d06
oscar <aaa>
2021-09-22 10:14:46 +0200

    asdasd

--- MOVILES/primeraApp/app/src/main/java/com/example/primeraapp/MainActivity.kt
+++ MOVILES/primeraApp/app/src/main/java/com/example/primeraapp/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.primeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val boton = this.findViewById<Button>(R.id.boton)
+        val etName = this.findViewById<EditText>(R.id.etName)
+        boton.text = "HOLA BOTON"
+//        ponerlisteners()
+        boton.setOnClickListener {
+            val texto = etName.text
+            Toast.makeText( this, " Hola ${texto}", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
+
+//    private fun ponerlisteners() {
+//        boton.setOnClickListener {
+//            val texto = etName.text
+//            Toast.makeText( this, " Hola ${texto}", Toast.LENGTH_SHORT).show()
+//
+//        }
+//    }
 
 }
