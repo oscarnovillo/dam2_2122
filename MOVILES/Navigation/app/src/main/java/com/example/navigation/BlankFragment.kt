c62f131e8f2a991e8f260fd136c172ca44a93298
pepe <oscar.novillo@gmail.com>
2021-12-03 00:29:19 +0100

    kkk

--- MOVILES/Navigation/app/src/main/java/com/example/navigation/BlankFragment.kt
+++ MOVILES/Navigation/app/src/main/java/com/example/navigation/BlankFragment.kt
@@ -5,20 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navigation.databinding.BlankFragmentBinding
+import com.example.navigation.databinding.FragmentBlankBinding
+import java.time.LocalDate
 
 class BlankFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var _binding : FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        _binding =  FragmentBlankBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener{
+            val action = BlankFragmentDirections.actionBlankFragmentToBlankFragment22(Persona(1,"mmm"))
+            findNavController().navigate(BlankFragmentDirections.actionBlankFragmentToBlankFragment22(Persona(1,"mmm")))
 
+        }
+
+    }
 }
--- MOVILES/Navigation/app/src/main/java/com/example/navigation/BlankFragment2.kt
+++ MOVILES/Navigation/app/src/main/java/com/example/navigation/BlankFragment2.kt
@@ -2,25 +2,67 @@ package com.example.navigation
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.onNavDestinationSelected
+import com.example.navigation.databinding.BlankFragmentBinding
+import com.example.navigation.databinding.FragmentBlankBinding
 
 class BlankFragment2 : Fragment() {
 
-
+    private var _binding : BlankFragmentBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: BlankViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.blank_fragment, container, false)
+        _binding =  BlankFragmentBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args : BlankFragment2Args by navArgs()
+        binding.texto.setText(args.persona?.nombre ?: "nulo")
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_dos, menu)
+    }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean =
+        when (menuItem.itemId) {
+            R.id.nuevo -> {
+                // Handle favorite icon press
+                //menuItem.onNavDestinationSelected(navController)
+                //findNavController().navigate(R.id.action_blankFragment_to_blankFragment22)
+
+                //findNavController().navigate()
+
+                Toast.makeText(context,"kkkk", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.nuevo2 -> {
+                //menuItem.onNavDestinationSelected(navController)
+                // Handle search icon press
+                true
+            }
+            else -> false
+        }
 
 
 }
