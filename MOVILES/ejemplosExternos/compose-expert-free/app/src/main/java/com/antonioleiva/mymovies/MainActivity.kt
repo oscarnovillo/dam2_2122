6b6ab3cd1779a91499b42b6b8fbb0c73c35d7ddf
pepe <oscar.novillo@gmail.com>
2022-02-01 23:44:54 +0100

    ggg

---
+++ MOVILES/ejemplosExternos/compose-expert-free/app/src/main/java/com/antonioleiva/mymovies/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.antonioleiva.mymovies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import coil.annotation.ExperimentalCoilApi
+import com.antonioleiva.mymovies.ui.navigation.Navigation
+
+@ExperimentalFoundationApi
+@ExperimentalCoilApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Navigation()
+        }
+    }
+}
